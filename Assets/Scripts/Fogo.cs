116d09610e51f859441c408a5335a42ba305959b
Hiroki Minami <rafael.hiroki@gmail.com>
2016-10-31 21:15:57 -0200

    Adding scripts for Caveira and Fogo

---
+++ Assets/Scripts/Fogo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fogo : MonoBehaviour {
+
+	private bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+		facingRight = false;
+	}
+
+	void FixedUpdate () {
+		HandleMovement ();
+	}
+
+	void HandleMovement() {
+
+		//Flip (horizontal);
+	}
+
+	private void Flip(float horizontal) {
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+			facingRight = !facingRight;
+
+			Vector2 spriteScale = transform.localScale;
+
+			spriteScale.x *= -1;
+
+			transform.localScale = spriteScale;
+		}
+	}
+
+}
